4f5c53e367327ebea9172d53d81aa9f7f4f327cd
BrandonShute <brandon.shute@kooltra.com>
2018-01-15 09:56:24 -0500

    Minor clean up for BlackScholes pricing

--- ValuationEngine/BlackScholes.py
+++ ValuationEngine/BlackScholes.py
@@ -5,77 +5,87 @@
 #
 # Analytical Black-Scholes-Merton (BSM) Formula
 
+import numpy as np
 from math import log, sqrt, exp
 from scipy import stats
 
 
-def bsm_value(S0, K, T, r, sigma, q, option):
+def black_scholes_value(index_price, strike, time, risk_free, sigma, div_yield, option_type):
     '''
     Valuation of European option and the Greeks in BSM model.
     Analytical formula.
 
     Parameters
     ==========
-    S0 : initial stock/index level
-    K : strike price
-    T : maturity date (in year fractions)
-    r : constant risk-free short rate
+    index_price : initial stock/index level
+    strike : strike price
+    time : maturity date (in year fractions)
+    risk_free : constant risk-free short rate
     sigma : volatility factor in diffusion term
-    q: continuously compounded dividend yield
-    option: call/put
+    div_yield: continuously compounded dividend yield
+    option_type: call/put
 
     Returns
     =======
     value : present value of the European call/put option
     '''
 
+    days_in_year = 365
+
     # ensure interest rate is non-negative
-    if r < 0:
-        r = 0
+    if risk_free < 0:
+        risk_free = 0
 
-    S0 = float(S0)
-    d1 = (log(S0 / K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-    d2 = (log(S0 / K) + (r - q - 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-    # stats.norm.cdf -> cumulative distribution function
-    # for normal distribution
+    index_price = float(index_price)
 
-    if option == 'call':
-        value = (S0 * exp(-q * T) * stats.norm.cdf(d1, 0.0, 1.0) - K * exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0))
+    d_plus = (log(index_price / strike) + (risk_free - div_yield + 0.5 * sigma ** 2) * time) / (sigma * sqrt(time))
+    d_minus = d_plus - (sigma * sqrt(time))
 
-        delta = exp(-q * T) * stats.norm.cdf(d1, 0.0, 1.0)
+    discount_factor_risk_free = exp(-risk_free * time)
+    discount_factor_div_yield = exp(-div_yield * time)
 
-        theta = (-(S0 * sigma * exp(-q * T) / (2 * sqrt(T)) * stats.norm.pdf(d1, 0.0, 1.0)) - r * K * exp(
-            -r * T) * stats.norm.cdf(d2, 0.0, 1.0) + q * S0 * exp(-q * T) * stats.norm.cdf(d1, 0.0, 1.0)) / (365 * T)
+    norm_pdf_d_plus = stats.norm.pdf(d_plus, 0.0, 1.0)
 
-        rho = 0.01 * K * T * exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0)
-
+    if option_type == 'call':
+        eta = 1
     else:
-        value = (K * exp(-r * T) * stats.norm.cdf(-d2, 0.0, 1.0) - S0 * exp(-q * T) * stats.norm.cdf(-d1, 0.0, 1.0))
+        eta = -1
 
-        delta = exp(-q * T) * (stats.norm.cdf(d1, 0.0, 1.0) - 1)
+    eta_norm_cdf_d_plus = stats.norm.cdf(eta * d_plus, 0.0, 1.0)
+    eta_norm_cdf_d_minus = stats.norm.cdf(eta * d_minus, 0.0, 1.0)
 
-        theta = (-(S0 * sigma * exp(-q * T) / (2 * sqrt(T)) * stats.norm.pdf(d1, 0.0, 1.0)) + r * K * exp(
-            -r * T) * stats.norm.cdf(-d2, 0.0, 1.0) - q * S0 * exp(-q * T) * stats.norm.cdf(-d1, 0.0, 1.0)) / (365 * T)
+    value = eta * (
+            index_price * discount_factor_div_yield * eta_norm_cdf_d_plus - strike * discount_factor_risk_free * eta_norm_cdf_d_minus)
 
-        rho = 0.01 * K * T * exp(-r * T) * stats.norm.cdf(-d2, 0.0, 1.0)
+    delta = eta * discount_factor_div_yield * eta_norm_cdf_d_plus
 
-    vega = 0.01 * S0 * exp(-q * T) * sqrt(T) * stats.norm.pdf(d1, 0.0, 1.0)
-    gamma = exp(-q * T) / (S0 * sigma * sqrt(T)) * stats.norm.pdf(d1, 0.0, 1.0)
+    gamma = discount_factor_div_yield / (index_price * sigma * sqrt(time)) * norm_pdf_d_plus
 
-    # return value
+    theta = (-(index_price * sigma * discount_factor_div_yield / (2 * sqrt(time)) * norm_pdf_d_plus) - eta * (
+            risk_free * strike * discount_factor_risk_free * eta_norm_cdf_d_minus + div_yield * index_price * discount_factor_div_yield * eta_norm_cdf_d_plus) / (
+            days_in_year * time))
+
+    rho = eta * (0.01 * strike * time * discount_factor_risk_free * eta_norm_cdf_d_minus)
+
+    vega = 0.01 * index_price * discount_factor_div_yield * sqrt(time) * norm_pdf_d_plus
 
     return {'value': value, 'delta': delta, 'theta': theta, 'rho': rho, 'vega': vega, 'gamma': gamma}
 
 
-def bsm_imp_vol(S0, K, T, r, C0, sigma_est, q, option, it):
+def calculate_implied_vol(index_price, strike, time, risk_free, option_price, sigma_est, div_yield, option_type,
+                          max_iterations=10000):
     '''
     Parameters
     ==========
+    index_price : initial stock/index level
+    strike : strike price
+    time : maturity date (in year fractions)
+    risk_free : constant risk-free short rate
+    option_price : the option's price
     sigma_est : estimate of impl. volatility
-    it : number of iterations
-    C0 : the option's price
-    q : continuously compounded dividend yield
-    option : call/put
+    div_yield : continuously compounded dividend yield
+    option_type : call/put
+    max_iterations : the maximum number of iterations
 
     Returns
     =======
@@ -85,32 +95,33 @@ def bsm_imp_vol(S0, K, T, r, C0, sigma_est, q, option, it):
     MAX_JUMP = 0.5
     TOLERENCE = 0.000000000000001
 
-    for i in range(it):
-        bsm_result = bsm_value(S0, K, T, r, sigma_est, q, option)
-        d_sigma = (bsm_result['value'] - C0) / (100 * bsm_result['vega'])
+    for i in range(max_iterations):
+        bsm_result = black_scholes_value(index_price, strike, time, risk_free, sigma_est, div_yield, option_type)
 
-        if d_sigma > MAX_JUMP:
-            d_sigma = MAX_JUMP
-        elif d_sigma < -1 * MAX_JUMP:
-            d_sigma = -1 * MAX_JUMP
+        price_difference = bsm_result['value'] - option_price
+        d_sigma = price_difference / (100 * bsm_result['vega'])
+
+        if d_sigma < TOLERENCE:
+            break
+        elif abs(d_sigma) > MAX_JUMP:
+            d_sigma = np.sign(d_sigma) * MAX_JUMP
 
         sigma_est -= d_sigma
+        sigma_est = max(sigma_est, 0)
 
-        if sigma_est < TOLERENCE:
-            sigma_est = TOLERENCE
 
     return sigma_est
 
 
-def bsm_imp_strike_from_vol(S0, sig_impl, T, r, q, delta):
+def get_implied_strike_from_implied_vol(index_price, sigma_implied, time, risk_free, div_yield, delta):
     '''
     Parameters
     ==========
-    S0 : initial stock/index level
-    sig_impl : implied volatility of the option
-    T : maturity date (in year fractions)
-    r : constant risk-free short rate
-    q: continuously compounded dividend yield
+    index_price : initial stock/index level
+    sigma_implied : implied volatility of the option
+    time : maturity date (in year fractions)
+    risk_free : constant risk-free short rate
+    div_yield: continuously compounded dividend yield
     delta: the delta of the option
 
     Returns
@@ -118,13 +129,12 @@ def bsm_imp_strike_from_vol(S0, sig_impl, T, r, q, delta):
     imp_strike : The implied strike calculated from delta
     '''
 
-    denom = exp(stats.norm.ppf(delta * exp(q * T)) * sig_impl * sqrt(T) - (r - q - 0.5 * (sig_impl ** 2)) * T)
+    denom = exp(stats.norm.ppf(delta * exp(div_yield * time)) * sigma_implied * sqrt(time) - (
+            risk_free - div_yield - 0.5 * (sigma_implied ** 2)) * time)
 
-    imp_strike = S0 / denom
+    imp_strike = index_price / denom
 
     return imp_strike
-
-    # bsm_value(S0, K, T, r, sigma, q, option)  # bsm_imp_vol(S0, K, T, r, C0, sigma_est, q, option, it)
 
 
 if __name__ == '__main__':
@@ -138,5 +148,5 @@ if __name__ == '__main__':
     it = 20
     option = 'call'
     sigma_est = 0.2
-    imp_vol = bsm_imp_vol(S0, K, T, r, C0, sigma_est, q, option, it)
+    imp_vol = calculate_implied_vol(S0, K, T, r, C0, sigma_est, q, option, it)
     print('Implied Vol: ' + str(imp_vol))
